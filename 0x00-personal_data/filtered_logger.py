a65a354dacfa351015ce39890b6f38d23f75e14d
Nadira3 <paitanun35@gmail.com>
2024-11-23 07:47:15 +0100

    task 2 submission

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,64 +8,74 @@ This module contains the filter_datum function, which obfuscates
 the values of specified fields in a given log message.
 """
 
-
 import logging
 import re
-from typing import List
+from typing import List, Tuple
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+PII_FIELDS: Tuple[str, ...] = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-    Obfuscates specified fields in a log message.                                                 
+    Obfuscates specified fields in a log message.
     Args:
         fields (List[str]): A list of field names to be obfuscated.
-        redaction (str): The string to replace the sensitive field values with.                           
+        redaction (str): The string to replace the sensitive field values with.
         message (str): The log message that contains the fields.
         separator (str): The separator character between fields in the message.
 
-    Returns:                                                                                              
+    Returns:
         str: The log message with sensitive field values replaced
         by the redaction string.
     """
     obfuscated_message = re.sub(
-        r'(' + '|'.join([re.escape(field) for field in fields]) + r')=[^' + re.escape(separator) + r']+',
+        r'(' + '|'.join([re.escape(field) for field in fields
+                         ]) + r')=[^' + re.escape(separator) + r']+',
         lambda m: m.group(0).split('=')[0] + '=' + redaction,
         message
     )
-    # Add spaces after semicolons except the last one
     parts = obfuscated_message.split(separator)
     return f"{separator} ".join(parts[:-1]) + separator + parts[-1]
 
 
 class RedactingFormatter(logging.Formatter):
-    """
-        Redacting Formatter class that filters
-        sensitive data from log messages.
-    """
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """
-        Initializes the formatter with the fields to filter.
-
-        Args:
-            fields (List[str]): A list of field names to be obfuscated
-            in log messages.
-        """
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """
-        Formats the log record, replacing specified fields
-        with redaction string.
-
-        Args:
-            record (logging.LogRecord): The log record to format.
-
-        Returns:
-            str: The formatted log message with filtered field values.
+        Format the log message, obfuscating sensitive fields.
         """
-        return super(RedactingFormatter, self).format(record).replace(record.msg, filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR))
+        original_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            original_message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and configures a logger named "user_data".
+    The logger will only log up to INFO level, and it will not propagate
+    messages to other loggers. It includes a StreamHandler with
+    a RedactingFormatter to filter sensitive information.
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(handler)
+
+    return logger
