0d7addda087f7b630f6ca4f1fd5a121d1d60ee23
Nadira3 <paitanun35@gmail.com>
2024-11-25 00:33:29 +0100

    modified:   api/v1/app.py to handle basic auth implementation

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -18,8 +19,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 # Load auth class
 auth = None
 auth_type = getenv('AUTH_TYPE', 'auth')
-if auth_type == 'auth':
-    auth = Auth()
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 
 @app.before_request
