cd3776f6f20977aea5f2c587a19c21590cb95256
Nadira3 <paitanun35@gmail.com>
2024-11-24 23:56:10 +0100

    modified:   api/v1/auth/auth.py; removed duplicate function

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,17 +15,3 @@ class Auth:
 
     def current_user(self, request=None) -> TypeVar('User'):
         return None
-
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        if path is None or excluded_paths is None or not excluded_paths:
-            return True
-
-        for excluded_path in excluded_paths:
-            if path.endswith('/'):
-                path = path[:-1]
-            if excluded_path.endswith('/'):
-                excluded_path = excluded_path[:-1]
-            if path == excluded_path:
-                return False
-
-        return True
