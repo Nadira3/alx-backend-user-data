6db4916707e001e335a99f4494f4b6fb72c4e25e
Nadira3 <paitanun35@gmail.com>
2024-11-24 23:36:28 +0100

    implemeted unauthorized endpoint

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,16 +14,16 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+@app.errorhandler(401)
+def unauthorized(error):
+    return jsonify({"error": "Unauthorized"}), 401
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
-
-@app.errorhandler(401)
-def unauthorized_error(error):
-    return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
 def forbidden_error(error):
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,6 +26,6 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/api/v1/unauthorized', methods=['GET'])
-def unauthorized_route():
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized_endpoint():
     abort(401)
