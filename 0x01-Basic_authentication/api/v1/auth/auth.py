c3ae48aceb233ab746c069424e336197e0e12474
Nadira3 <paitanun35@gmail.com>
2024-11-25 00:10:29 +0100

    require_auth function expanded to handle paths and excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,16 @@ class Auth:
     """ Authentication class """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ require auth """
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        for excluded_path in excluded_paths:
+            if excluded_path[-1] != '/':
+                excluded_path += '/'
+            if path == excluded_path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ authorization header """
