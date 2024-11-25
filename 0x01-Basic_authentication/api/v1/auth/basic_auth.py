d8ac10244afcf983cc44bfb2b4e16171913f0ecb
Nadira3 <paitanun35@gmail.com>
2024-11-25 01:00:22 +0100

    In api/v1/auth/basic_auth.py, implement the user_object_from_credentials method:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,7 +5,9 @@ Basic Auth module for the API
 
 
 import base64
+from models.user import User
 from api.v1.auth.auth import Auth
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -46,3 +48,20 @@ class BasicAuth(Auth):
             return None, None
         email, password = decoded_base64_authorization_header.split(':', 1)
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """
+        Returns a User instance based on email and password
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            user = User.search({'email': user_email})
+            if not user or not user[0].is_valid_password(user_pwd):
+                return None
+            return user[0]
+        except Exception:
+            return None
